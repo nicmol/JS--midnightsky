8e87dfd80e2567d289fe2ee6966baa77f7d7a5ec
goodmari <goodm@lanecc.edu>
2018-04-09 06:36:33 -0700

    Added detailed instructions

--- src/js/weather.js
+++ src/js/weather.js
@@ -2,8 +2,7 @@ import './general';
 
 //http://api.openweathermap.org/data/2.5/forecast/daily?zip=97405&units=imperial&appid=c59493e7a8643f49446baf0d5ed9d646
 
-/*
-Create a class called Weather
+/* Create a class called Weather
 - Part 1 - Retrieve the weather information when the user clicks the buttobn
   - Create the constructor
     - initialize instance variables for the "state" of the app and the ajax call
@@ -60,127 +59,22 @@ END OF PART 1 - TEST AND DEBUG YOUR APP
       - that contains the itemsHTML from above
 END OF PART 2 - TEST AND DEBUG YOUR APP
 - Part 3 - Display weather details when the user clicks one weather list item
-
-  add event handler that makes the api call - just log the results to the console
-  write renderWeatherList
-  write renderWeatherListItem
+  - Write the method renderCurrentDay.  It takes the index of the day as it's parameter.
+    - Format the detailed weather information for the selected day on the html page. Include at least
+      - identifying information for the city as well as the date
+      - description and icon for the weather
+      - temperatures throughout the day
+      - humidity and wind information
+    - CUT the html for the weather details and paste it into the body of your method
+      - Enclose the html in ``.
+      - Replace the hardcoded text with data.  The data is in the state instance variable.
+      - Set the innerhtml property of the currentDay element on the page
+  - Add a click event handler to each of the weather list items 
+    - add a loop to the end of the renderWeatherList method that adds the event handler
+    - you'll have to bind the method renderCurrentDay to both the class and the index of the item
+  - Write the method clearCurrentDay.  It sets the inner html property of the currentDay element to ""
+  - Call clearCurrentDay at the end of onFormSubmit
+END OF PART 3 - TEST AND DEBUG YOUR APP
 */
 
-class Weather {
-  constructor() {
-    this.state = {
-      zipcode: "",
-      city: {},
-      dates: [],
-      selectedDate: null
-    };
-    this.url = "http://api.openweathermap.org/data/2.5/forecast/daily?zip=";
-    this.apikey = "&units=imperial&appid=c59493e7a8643f49446baf0d5ed9d646";
-
-    this.$form = document.querySelector('#zipForm');
-    this.$zipcode = document.querySelector('#zipcode');
-    this.$weatherList = document.querySelector('#weatherList');
-
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.renderWeatherList = this.renderWeatherList.bind(this);
-    this.renderWeatherlistItem = this.renderWeatherListItem.bind(this);
-    this.$form.addEventListener('submit', this.onFormSubmit);
-
-    this.$currentDay = document.querySelector('#currentDay');
-    this.addItemClickHandlers = this.addItemClickHandlers.bind(this);
-    this.clearCurrentDay = this.clearCurrentDay.bind(this);
-  }
-
-  onFormSubmit(event) {
-    event.preventDefault();
-    this.state.zipcode = this.$zipcode.value;
-    fetch(`${this.url}${this.state.zipcode}${this.apikey}`)
-    .then(response => response.json())
-    .then(data => { 
-      //const {city, list: dates } = data;
-      this.state.city = data.city;
-      this.state.dates = data.list;
-      this.state.selectedDate = null;
-      this.$zipcode.value = "";
-      this.renderWeatherList(this.state.dates);
-      this.clearCurrentDay();
-      this.addItemClickHandlers();
-    })
-    .catch(error => {
-        alert('There was a problem getting info!'); 
-    }); 
-  }
-
-  renderWeatherList(days) {
-    const itemsHTML = days.map((day, index) => this.renderWeatherListItem(day, index)).join('');
-    this.$weatherList.innerHTML = 
-      `<div class="weather-list flex-parent">
-          ${itemsHTML}
-      </div>`;
-  }
-
-  addItemClickHandlers() {
-    const items = document.querySelectorAll('.weather-list-item');
-    for (let i = 0; i < items.length; i++)
-      items[i].onclick = this.renderCurrentDay.bind(this, [i]);
-  }
-
-  getWeekday(date) {
-    const dayNames = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const weekday = date.getDay();
-    return dayNames[weekday];
-  }
-
-  renderWeatherListItem(day, index) {
-    const date = new Date(day.dt * 1000);
-    const weekday = this.getWeekday(date);
-    const city = this.state.city;
-    return `
-        <div class="weather-list-item" data-index="${index}">
-            <h2>${date.getMonth() + 1} / ${date.getDate()}</h2>
-            <h3>${weekday}</h3>
-            <h3>${day.temp.min.toFixed(1)}&deg;F &#124; ${day.temp.max.toFixed(1)}&deg;F</h3>
-        </div>
-    `;
-  }
-
-  renderCurrentDay(params) {
-    const index = params[0];
-    const city = this.state.city;
-    const day = this.state.dates[index];
-    const w = day.weather[0];
-    const date = new Date(day.dt * 1000);
-    const weekday = this.getWeekday(date);
-    const dayHTML = `
-      <div class="current-day">
-        <h1 class="day-header">${weekday} in ${city.name}</h1>
-        <div class="weather">
-        <p>
-            <img src='http://openweathermap.org/img/w/${w.icon}.png' alt='${w.description}'/>
-            ${w.description}
-        </p>
-      </div>
-      <div class="details flex-parent">
-        <div class="temperature-breakdown">
-          <p>Morning Temperature: ${day.temp.morn}&deg;F</p>
-          <p>Day Temperature: ${day.temp.day}&deg;F</p>
-          <p>Evening Temperature: ${day.temp.eve}&deg;F</p>
-          <p>Night Temperature: ${day.temp.night}&deg;F</p>
-        </div>
-        <div class="misc-details">
-          <p>Atmospheric Pressure: ${day.pressure} hPa</p>
-          <p>Humidity: ${day.humidity}%</p>
-          <p>Wind Speed: ${day.speed} mph</p>
-        </div>
-      </div>
-    </div>
-    `;
-    this.$currentDay.innerHTML = dayHTML;
-  }
-
-  clearCurrentDay() {
-    this.$currentDay.innerHTML = "";
-  }
-}
-
-new Weather();
+// Don't forget to instantiate the a weather object!
